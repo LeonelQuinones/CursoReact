5d64d2387aef0da460215c628a9bdcf754dfd39f
leonelquinones <leonelquinones9@gmail.com>
2022-05-12 23:54:04 -0300

    desafio n2

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <img src="/coca-cola-logo-1.png" width="90" />
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav mx-auto fs-5">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Productos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Empresa</a>
+                        </li>
+                    </ul>
+                </div>
+                <img src="/carrito-compras.png" width="70" />
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
